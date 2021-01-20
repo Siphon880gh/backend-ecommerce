19a439b0ccbc86db63f19f6f745b9cc8414ceacc
Weng Fei Fung <weffung@ucdavis.edu>
2021-01-20 02:32:50 -0800

    Category Model: Add more columns

--- models/Category.js
+++ models/Category.js
@@ -4,17 +4,23 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
-  },
-  {
+Category.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+});
 
 module.exports = Category;
